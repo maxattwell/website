a82642765c2296fc850978d008333538194fe2ef
Max Attwell <max.attwell@hotmail.com>
2025-05-08 22:20:12 +1200

    add pages workflow and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://maxattwell.github.io',
+  base: '/website/',
   integrations: [react()]
+  output: 'static'
 });
