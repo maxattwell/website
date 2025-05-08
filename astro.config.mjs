1f46a7ac496d0e51257164adbff53e99ad6df968
Max Attwell <max.attwell@hotmail.com>
2025-05-08 22:26:05 +1200

    update according to astro docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maxattwell.github.io',
-  base: '/website/',
+  base: 'website',
   integrations: [react()]
   output: 'static'
 });
