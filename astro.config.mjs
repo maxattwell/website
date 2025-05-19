aaab74e1760cbcc587ff165931ad06dac568c750
Max Attwell <max.attwell@hotmail.com>
2025-05-19 21:12:29 +1200

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://maxattwell.github.io',
   base: 'website',
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
